4d28b7dce9bd565060014fe0562ba447f82514c6
jjudy_kim <imjubaaakim@gmail.com>
2021-08-13 19:29:51 +0900

    11~20 done.

---
+++ 11_20.py
@@ -0,0 +1,51 @@
+#011
+삼성전자 = 50000
+총_평가금액 = 10 * 삼성전자
+print(총_평가금액)
+
+#012
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액)
+print(현재가)
+print(PER)
+
+#013
+s = "hello"
+t = "python"
+print(f"{s}! {t}")
+
+#014
+print(2 + 2 * 3)
+
+#015
+a = "132"
+print(type(a))
+
+#016
+num_str = "720"
+print(int(num_str))
+print(type(int(num_str)))
+
+#017
+num = 100
+print(str(num))
+print(type(str(num)))
+
+#018
+numf_str = "15.79"
+print(float(numf_str))
+print(type(float(numf_str)))
+
+#019
+year = "2020"
+int_year = int(year)
+for i in range(int_year-3, int_year):
+    print(i + 1)
+
+#020
+monthly_fee = 48584
+total_fee = monthly_fee * 36
+print(total_fee)
+
