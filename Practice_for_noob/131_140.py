ef2fe3a5fa460d79d1f453fea69f4619ff85cc62
jjudy_kim <imjubaaakim@gmail.com>
2021-08-24 09:16:00 +0900

    131~140 done.

---
+++ Practice_for_noob/131_140.py
@@ -0,0 +1,53 @@
+#131
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+    print(변수)
+
+#132
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+    print("#####")
+
+#133
+print("A")
+print("B")
+print("C")
+
+#134
+print("출력:", "A")
+print("출력:", "B")
+print("출력:", "C")
+
+#135
+변수 = "A"
+b = 변수.lower()
+print("변환:", b)
+변수 = "B"
+b = 변수.lower()
+print("변환:", b)
+변수 = "C"
+b = 변수.lower()
+print("변환:", b)
+
+#136
+변수 = [10, 20, 30]
+for i in 변수:
+    print(변수)
+
+#137
+for a in [10, 20, 30]:
+    print(a)
+
+#138
+for a in [10, 20, 30]:
+    print(a)
+    print("---------")
+
+#139
+print("++++")
+for a in [10, 20, 30]:
+    print(a)
+
+#140
+for a in range(4):
+    print("---------")
